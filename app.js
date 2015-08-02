6e7f36b0d3ea741736818d926f93bca33e50b7e6
Libertual <peterworld@gmail.com>
2015-08-02 21:21:40 +0200

    Auto-logout (M9)

--- app.js
+++ app.js
@@ -29,6 +29,22 @@ app.use(session());
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
+// MW auto logout
+
+app.use(function(req, res, next) {
+  console.log("session.time: "+ req.session.time + "\nDate.now(): "+ Date.now());
+
+  if(req.session && req.session.time < (Date.now() - 10000))
+  {
+	console.log("Destruida");
+	delete req.session.user;
+  }
+ else req.session.time = Date.now();
+  next();
+});
+
 // Helpers dinamicos:
 app.use(function(req, res, next) {
 
@@ -41,6 +57,7 @@ app.use(function(req, res, next) {
   res.locals.session = req.session;
   next();
 });
+
 
 app.use('/', routes);
 
